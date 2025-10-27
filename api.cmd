1c3d3649142ee530c8d6760f498fc739266a10af
tasqlab <tchazq1n@gmail.com>
2025-10-28 09:58:11 +1100

    Add files via upload

---
+++ api.cmd
@@ -0,0 +1,5 @@
+::put your api there
+::go to https://aistudio.google.com/app/api-keys for your api
+::if you do not have one, make one 🤯
+::its free (500 prompts per day)
+setx GEMINI_API_KEY "your api key here"
